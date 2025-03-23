4654563b0a58af7f7af89fdfd9121e4bd0323b4c
Glenn Vandeuren (aka Iondependent) <vandeurenglenn@gmail.com>
2025-03-23 10:10:49 +0100

    Create rollup.config.js

---
+++ rollup.config.js
@@ -0,0 +1,20 @@
+import typescript from '@rollup/plugin-typescript'
+import resolve from '@rollup/plugin-node-resolve'
+import { copyFile, cp } from 'fs/promises'
+
+try {
+  await copyFile('src/index.html', 'www/index.html')
+} catch (error) {}
+
+try {
+  await cp('node_modules/@vandeurenglenn/lite-elements/exports/themes', 'www/themes', { recursive: true })
+} catch (error) {}
+
+export default {
+  input: ['./src/shell.ts'],
+  output: {
+    dir: 'www',
+    format: 'es'
+  },
+  plugins: [typescript(), resolve()]
+}
